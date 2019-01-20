eab96b5d23d728f0d2c65a577f7179ab17d4ee5e
Shefass <shefass@gmail.com>
2019-01-20 13:57:35 +0200

    Updates

---
+++ precache-manifest.180ef64278e194e6e420078ab19325c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11a7a1d07afa29cdfb1",
+    "url": "/quote/static/js/runtime~main.d11a7a1d.js"
+  },
+  {
+    "revision": "a298320876794c973be0",
+    "url": "/quote/static/js/main.a2983208.chunk.js"
+  },
+  {
+    "revision": "96cc866f369b11afbcff",
+    "url": "/quote/static/js/1.96cc866f.chunk.js"
+  },
+  {
+    "revision": "a298320876794c973be0",
+    "url": "/quote/static/css/main.ea0cde71.chunk.css"
+  },
+  {
+    "revision": "91fc25095e948cf4e393e4725bd7db7e",
+    "url": "/quote/index.html"
+  }
+];
